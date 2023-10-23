2499cb0c4b5aa7759f65359dda2e085c91712db5
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-10-23 23:27:44 +0300

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 import { isProduction } from '~/utils/helpers'
 
 export default defineAppConfig({
-    baseURL: isProduction ? '/nuxtjs-ithra-admin/' : '/',
+    baseURL: isProduction ? 'https://ithra-2023.github.io/nuxtjs-ithra-admin/' : '/',
     buildAssetsDir: 'assets',
 })
